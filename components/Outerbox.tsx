dafee7d0588ecd71b2ad1af0fcbec274bcafd276
Byeong Gyu Kim <qudrb6989@postech.ac.kr>
2022-08-22 13:05:04 +0900

    new

---
+++ components/Outerbox.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+const Outerbox = () => {
+    return(
+        <div className='border-solid text-red-500'>
+            Hello sfasfasdfasdf
+        </div>
+    )
+}
+
+export default Outerbox;
